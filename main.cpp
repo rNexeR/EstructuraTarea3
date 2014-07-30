c6efdaa8b4180a59151ebe419e1ad87342725b7c
Turupawn <turupawn@userr>
2014-07-30 09:46:36 -0600

    primero commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include "Evaluador.h"
+#include <iostream>       // std::cin, std::cout
+#include <vector>          // std::list
+#include <set>          // std::list
+#include <map>          // std::list
+using namespace std;
+
+
+//agrega 'valor' a 'mi_set'
+void agregarASet(set<int> *mi_set,int valor)
+{
+}
+
+//agrega 'valor' a 'mi_map' usando la 'llave' como llave
+void agregarMap(map<string, int> *mi_map, string llave, int valor)
+{
+}
+
+//devuelve cuantos elementos de 'mi_multiset' tienen un valor de 'valor'
+int contarElementosMultiset(multiset<int> mi_multiset,int valor)
+{
+    return -1;
+}
+
+//devuelve cuantos elementos de 'mi_multimap' usan 'llave' como llave
+int contarElementosMultimap(multimap<string,int> mi_multimap,string llave)
+{
+    return -1;
+}
+
+//devuelve la suma de los elementos de 'mi_multiset' que usan 'llave' como llave
+int sumaElementosMultiset(multiset<int> mi_multiset, int valor)
+{
+    return -1;
+}
+
+//devuelve la suma de los elementos de 'mi_multimap' que usan 'llave' como llave
+int sumaElementosMultimap(multimap<string, int> mi_multimap, string llave)
+{
+    return -1;
+}
+
+//el promedio de todos los elementos que tiene 'mi_multiset' (incluyendo valores repetidos)
+int promedioElementosMultiset(multiset<int>mi_multiset)
+{
+    -1;
+}
+
+int main ()
+{
+
+    //Funcion evaluadora
+    evaluar();
+    return 1;
+}
